7eadc0a43cf9be5cf72e6c308b55da29eebe148a
Leonardo Soares <leonardogcsoares93@gmail.com>
2016-03-12 16:03:00 -0300

    12/3/2016 commit

--- client/mvpOne/mvpOne.component.js
+++ client/mvpOne/mvpOne.component.js
@@ -9,6 +9,28 @@ angular.module('abstrata').directive('mvpOne', function () {
                $document.scrollToElement(angular.element(document.getElementById(scrollElement)),
                     0, 500);
            };
+
+           $(function () {
+               $("#typed-changing").typed({
+                   strings: ['matemática', 'estatística', 'analise de dados'],
+                   typeSpeed: 25,
+                   loop: true,
+                   backDelay: 2000,
+                   showCursor: false,
+                   onStringTyped: function () {
+                       var currentText = $("#typed-changing").text().toString();
+                       var nextBackgroundToLoad = currentText === this.strings[0] ? "url(/media/img/math-background.jpg)" :
+                            currentText === this.strings[1] ? "url(/media/img/statistics-background.jpg)" : "url(/media/img/data-background.jpg)";
+                       $("#background-home").fadeOut("slow", function () {
+                           $("#background-home").css("background-image", nextBackgroundToLoad);
+                           $("#background-home").fadeIn("slow");
+                       });
+
+                   }
+
+
+               });
+           });
        }
    }
 });
