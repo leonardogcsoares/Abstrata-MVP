e761a3c687f2c4eaf69cb0b8a6c0a24fbeac529f
Arthur Nunes <apple@apples-MacBook-Air.local>
2016-03-17 17:01:45 -0300

    ...

--- client/mvpOne/mvpOne.component.js
+++ client/mvpOne/mvpOne.component.js
@@ -17,20 +17,25 @@ angular.module('abstrata').directive('mvpOne', function () {
                    loop: true,
                    backDelay: 2000,
                    showCursor: false,
+                   preStringTyped: function () {
+                     console.log("Before typing given string");
+                   },
                    onStringTyped: function () {
-                       var currentText = $("#typed-changing").text().toString();
-                       var nextBackgroundToLoad = currentText === this.strings[0] ? "url(/media/img/math-background.jpg)" :
-                            currentText === this.strings[1] ? "url(/media/img/statistics-background.jpg)" : "url(/media/img/data-background.jpg)";
-                       $("#background-home").fadeOut("slow", function () {
-                           $("#background-home").css("background-image", nextBackgroundToLoad);
-                           $("#background-home").fadeIn("slow");
-                       });
+                       // var currentText = $("#typed-changing").text().toString();
+                       // var nextBackgroundToLoad = currentText === this.strings[0] ? "url(/media/img/math-background.jpg)" :
+                       //      currentText === this.strings[1] ? "url(/media/img/statistics-background.jpg)" : "url(/media/img/data-background.jpg)";
+                       // $("#background-home").fadeOut("slow", function () {
+                       //     $("#background-home").css("background-image", nextBackgroundToLoad);
+                       //     $("#background-home").fadeIn("slow");
+                       // });
 
                    }
 
 
                });
            });
+           
+           
        }
    }
 });
